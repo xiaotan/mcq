a5331f8719987470b39b6d21186c93774e3e81b5
xiaotan <108437444@qq.com>
2017-05-17 11:49:20 +0800

    11

--- application/common.php
+++ application/common.php
@@ -10,7 +10,29 @@
 // +----------------------------------------------------------------------
 
 use think\Db;
+use think\Config;
+use Wechat\Loader;
 // 应用公共文件
+
+
+if (!function_exists('load_wechat')) {
+    /**
+     * 获取微信操作对象
+     * @staticvar array $wechat
+     * @param type $type
+     * @return WechatReceive
+     */
+    function & load_wechat($type = '') {
+        static $wechat = array();
+        $index = md5(strtolower($type));
+        if (!isset($wechat[$index])) {
+            $config = Config::get('wechat');
+            $config['cachepath'] = CACHE_PATH . 'Data/';
+            $wechat[$index] = Loader::get($type, $config);
+        }
+        return $wechat[$index];
+    }
+}
 
 if (!function_exists('is_signin')) {
     /**
---
+++ application/extra/wechat.php
@@ -0,0 +1,13 @@
+<?php 
+
+return [
+    'token'          => '', //填写你设定的token
+    'appid'          => '', //填写高级调用功能的app id, 请在微信开发模式后台查询
+    'appsecret'      => '', //填写高级调用功能的密钥
+    'encodingaeskey' => '', //填写加密用的EncodingAESKey（可选，接口传输选择加密时必需）
+    'mch_id'         => '',  //微信支付，商户ID（可选）
+    'partnerkey'     => '',  //微信支付，密钥（可选）
+    'ssl_cer'        => '', //微信支付，双向证书（可选，操作退款或打款时必需）
+    'ssl_key'        => '',  //微信支付，双向证书（可选，操作退款或打款时必需）
+    'cachepath'      => '', //设置SDK缓存目录（可选，默认位置在./Wechat/Cache下，请保证写权限）
+];
